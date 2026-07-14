c5676fb973f36ee38ff2d52984903cb01407ab72
WILLAM56585 <157664152+WILLAM56585@users.noreply.github.com>
2026-07-14 09:03:12 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -1,1 +1,123 @@
+const produtos = [
 
+{
+id:1,
+nome:"Vestido Floral Elegance",
+categoria:"Vestidos",
+preco:149.90,
+precoAntigo:219.90,
+desconto:30,
+imagem:"img/IMG-20260714-WA0021.jpg",
+tamanhos:["P","M","G","GG"],
+descricao:"Vestido elegante, tecido leve e confortável, perfeito para festas e ocasiões especiais."
+},
+
+{
+id:2,
+nome:"Blusa Premium Rosa",
+categoria:"Blusas",
+preco:89.90,
+precoAntigo:119.90,
+desconto:20,
+imagem:"img/IMG-20260714-WA0023.jpg",
+tamanhos:["PP","P","M"],
+descricao:"Blusa feminina premium com tecido macio e excelente acabamento."
+},
+
+{
+id:3,
+nome:"Conjunto Fashion",
+categoria:"Conjuntos",
+preco:229.90,
+precoAntigo:329.90,
+desconto:30,
+imagem:"img/IMG-20260714-WA0022.jpg",
+tamanhos:["P","M","G"],
+descricao:"Conjunto moderno ideal para passeios e eventos."
+},
+
+{
+id:4,
+nome:"Calça Jeans Feminina",
+categoria:"Calças",
+preco:139.90,
+precoAntigo:189.90,
+desconto:26,
+imagem:"img/IMG-20260714-WA0025.jpg",
+tamanhos:["38","40","42","44"],
+descricao:"Calça jeans com modelagem confortável."
+},
+
+{
+id:5,
+nome:"Short Jeans",
+categoria:"Shorts",
+preco:79.90,
+precoAntigo:99.90,
+desconto:20,
+imagem:"img/IMG-20260714-WA0020.jpg",
+tamanhos:["36","38","40"],
+descricao:"Short jeans feminino de cintura alta."
+},
+
+{
+id:6,
+nome:"Vestido Longo Luxo",
+categoria:"Vestidos",
+preco:189.90,
+precoAntigo:259.90,
+desconto:27,
+imagem:"img/IMG-20260714-WA0018.jpg",
+tamanhos:["M","G","GG"],
+descricao:"Vestido longo elegante para ocasiões especiais."
+},
+
+{
+id:7,
+nome:"Blazer Feminino",
+categoria:"Blazers",
+preco:249.90,
+precoAntigo:329.90,
+desconto:24,
+imagem:"img/IMG-20260714-WA0026.jpg",
+tamanhos:["P","M","G"],
+descricao:"Blazer feminino moderno e sofisticado."
+},
+
+{
+id:8,
+nome:"Conjunto Casual",
+categoria:"Conjuntos",
+preco:159.90,
+precoAntigo:219.90,
+desconto:27,
+imagem:"img/IMG-20260714-WA0016.jpg",
+tamanhos:["P","M","G","GG"],
+descricao:"Conjunto casual confortável para o dia a dia."
+},
+
+{
+id:9,
+nome:"Saia Jeans",
+categoria:"Saias",
+preco:99.90,
+precoAntigo:139.90,
+desconto:29,
+imagem:"img/IMG-20260714-WA0019.jpg",
+tamanhos:["36","38","40","42"],
+descricao:"Saia jeans moderna e confortável."
+},
+
+{
+id:10,
+nome:"Blusa Luxo",
+categoria:"Blusas",
+preco:109.90,
+precoAntigo:149.90,
+desconto:27,
+imagem:"img/IMG-20260714-WA0024.jpg",
+tamanhos:["PP","P","M","G"],
+descricao:"Blusa feminina com acabamento premium."
+}
+
+];
